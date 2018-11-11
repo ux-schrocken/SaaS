237b2e57b5068b2232d1052af254164bb9c80fa2
ux-schrocken <ux@schrocken.com>
2018-11-12 00:09:06 +0530

    added sidebar with bg-img,& sign in card

--- schrocken-web-app/src/App.js
+++ schrocken-web-app/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/login.component';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
-        <Login/>
+        <Login />
       </div>
     );
   }
